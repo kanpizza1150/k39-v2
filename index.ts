2f001c1ecb0c152d3e1f7ba09313b2bb3cd7c417
Kanpizza <kanpizza1150@gmail.com>
2022-03-27 17:40:02 +0700

    remove unused files and code

--- index.ts
+++ index.ts
@@ -10,14 +10,14 @@ const port = process.env.PORT || 8000
 const mongodbUrl = process.env.MONGODB_URL
 console.log('+++++++++++++++++++++++++++++mongodbUrl', mongodbUrl)
 mongoose.connect(mongodbUrl, { useNewUrlParser: true })
-const database = mongoose.connection
-database.on('error', (error: Error) => {
-  console.error('[DB] Error:', error)
-})
+// const database = mongoose.connection
+// database.on('error', (error: Error) => {
+//   console.error('[DB] Error:', error)
+// })
 
-database.once('connected', () => {
-  console.log('[DB]: Database Connected')
-})
+// database.once('connected', () => {
+//   console.log('[DB]: Database Connected')
+// })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
